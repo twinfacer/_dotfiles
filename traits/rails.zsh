04229bdd26f48553cf7b794fc88032b8b75248c7
Sergey Ivannikov <twinfacer@gmail.com>
2018-09-12 17:57:05 +0300

    remove with_data from rdm alias

--- traits/rails.zsh
+++ traits/rails.zsh
@@ -2,7 +2,7 @@
 alias rc="rails console"
 alias rs="rails server"
 alias rr="rails routes"
-alias rdm="rails db:migrate:with_data"
+alias rdm="rails db:migrate"
 alias rgm="rails generate migration"
 
 alias cov="firefox coverage/index.html >/dev/null"
