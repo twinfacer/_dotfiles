3f8771b9cff1fdd9ef0ba8d2349fad4d41af80df
Sergey Ivannikov <twinfacer@gmail.com>
2019-02-11 12:03:08 +0300

    WIP rvm & archlinux strains

--- strains/archlinux.zsh
+++ strains/archlinux.zsh
@@ -1,64 +1,52 @@
-# Sync time
+USERNAME=user
+HOSTNAME=host
+TZ="Europe/Moscow"
+# BAREBONE ARCHLINUX INSTALL
+# 1) Select drive to use
+# 2) Partition drive && swap
+# 3) Select timezone
+# 4) Select locale
+# 4) Select hostname
+# sync time
 timedatectl set-ntp true
-
-# Partitions
+# partitions
+# TODO: Automate
 cfdisk
-
 # Create filesystems
+# TODO: Can warry
 mkfs.ext4 /dev/sda1
-
 # Swap
+# TODO: Can warry
 mkswap /dev/sda2
 swapon /dev/sda2
-
 # Mount filesystem
 mount /dev/sda1 /mnt
-
 # Install packages
-pacstrap /mnt base base-devel grub git zsh sudo tmux
-
+# TODO: Probably some packages can be moved to 'future' stages
+pacstrap /mnt base base-devel grub # like here: git zsh sudo tmux
 # Generate fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
-
 # Chroot
 arch-chroot /mnt
-
 # Timezone setup
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
 hwclock --systohc
-
 # Locales
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
 # Network
-echo "wanderworld" > /etc/hostname
-
+echo $HOSTNAME > /etc/hostname
 # root password
 passwd
-
 # grub setup
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
-
 # POSTINSTALL
-
 # user
-useradd -m -G=wheel -s=/usr/bin/zsh wanderer
-passwd wanderer
+useradd -m -G=wheel -s=/usr/bin/zsh $USERNAME
+passwd $USERNAME
 
 # passwordless sudo
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-# yaourt
-git clone https://aur.archlinux.org/package-query.git
-cd package-query
-makepkg -si
-cd ..
-git clone https://aur.archlinux.org/yaourt.git
-cd yaourt
-makepkg -si
-cd ..
-rm -rf {package-query, yourt}
--- strains/rvm.zsh
+++ strains/rvm.zsh
@@ -1,1 +1,7 @@
+# Installation
 \curl -sSL https://get.rvm.io | bash -s stable
+
+# Enable completitions for zsh
+mkdir -p $HOME/.zsh/Completion
+cp $rvm/scripts/zsh/Completion/_rvm $HOME/.zsh/Completion
+echo 'fpath=(~/.rvm/scripts/zsh/Completion $fpath)' >> $HOME/.zshrc
