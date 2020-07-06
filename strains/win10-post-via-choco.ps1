8869838fbaa19672b9e92daf2060ebdca7329600
Sergey Ivannikov <twinfacer@gmail.com>
2020-07-06 21:15:03 +0300

    Delete win10-post-via-choco.ps1
    Remove win-10 chocolatey post install script

--- strains/win10-post-via-choco.ps1
+++
@@ -1,9 +0,0 @@
-## Chocolatey Post Instal Packsges
-$packages = @(
-  firefox tor deluge openvpn atom cygwin github_desktop
-  punto_switcher peazip vlc aimp gimp steam java
-)
-
-foreach ($package in $packages) {
-  choko install $package
-}
