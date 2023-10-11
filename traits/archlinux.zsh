c09c211443a830b1d05bb56073687816736a6a7b
Sergey Ivannikov <twinfacer@gmail.com>
2023-10-11 11:50:23 +0300

    Update archlinux.zsh

--- traits/archlinux.zsh
+++ traits/archlinux.zsh
@@ -3,6 +3,7 @@ which pacman &>/dev/null || return
 # pacman (pac*)
 alias pacl="pacman -Q"
 alias pacl!="pacman -Qe"
+alias pacle='pacman -Qent'
 alias pacs="pacman -Qo"
 alias pacr="sudo pacman -Rsu"
 alias pacu="sudo pacman -Suyy"
