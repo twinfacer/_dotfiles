84b4689eb7f006002f11301e3c25f73ec72a1dea
Sergey Ivannikov <twinfacer@gmail.com>
2024-01-30 14:25:58 +0300

    Update postgresql.zsh

--- traits/postgresql.zsh
+++ traits/postgresql.zsh
@@ -4,3 +4,4 @@ _create_pg_superadmin() { _exec_as_postgres "createuser -d -P -s $1" }
 
 alias pge="_exec_as_postgres"
 alias pgu!="_create_pg_superadmin"
+alias pginit="su - postgres -c \"initdb --locale en_US.UTF-8 -D '/var/lib/postgres/data' &>/dev/null\""
