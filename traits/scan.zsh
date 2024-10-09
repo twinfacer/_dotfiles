2507478d0a1151f4889982ff2fc3fd338b345b41
Sergey Ivannikov <twinfacer@gmail.com>
2024-10-09 18:39:42 +0300

    Update scan.zsh

--- traits/scan.zsh
+++ traits/scan.zsh
@@ -1,3 +1,13 @@
+# Reverse DNS lookup
 revdns() {
+  callable pup || (echo "[!] pup not installed!" && return)
   curl https://rapiddns.io/sameip/$1#result -s | pup "tbody tr td:nth-of-type(1) text{}" 
+}
+
+# Scan for active subdomains using _subfinder_ & _httpx_.
+subz() {
+  callable subfinder || (echo '[!] subfinder not installed!' && return)
+  callable httpx || (echo '[!] https not installed!' && return)
+
+  subfinder -silent -d $1 2>/dev/null | httpx -silent | cut -d '/' -f 3 | sort -u
 }
